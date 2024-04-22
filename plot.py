5a9db6582c4c8986ad631b6c7365d9efedf4db50
JuliuszZiomek <juliusz.ziomek@gmail.com>
2024-04-22 18:34:43 +0000

    Added smoothing

--- plot.py
+++ plot.py
@@ -6,6 +6,7 @@ import pickle
 import json
 import argparse
 import seaborn as sns
+from scipy.ndimage import gaussian_filter1d
 
 parser = argparse.ArgumentParser()
 parser.add_argument("runs_file")
@@ -14,9 +15,7 @@ args = parser.parse_args()
 runs_file = args.runs_file
 legend = args.legend
 
-
 run_hashes = json.load(open(runs_file, 'rb'))
-
 
 df = pd.DataFrame()
 for run_name, run_dir in run_hashes.items():
@@ -36,7 +35,14 @@ for run_name, run_dir in run_hashes.items():
             )
         ])
 
-sns.lineplot(df, y="Eval Returns", x="N", hue='algorithm', style='algorithm', errorbar=("se", 1), legend="auto" if legend else False)
-plt.savefig(runs_file[:-4] + ".png")
-print(f"Saved results in {runs_file[:-4]}.png .")
+# Smooth the data using a 1D filter
+smoothed_returns = df.groupby(['algorithm', 'seed'])['Eval Returns'].transform(lambda x: gaussian_filter1d(x, sigma=5))
 
+# Add smoothed data to DataFrame
+df['Smoothed Returns'] = smoothed_returns
+
+# Plot with smoothed data and confidence intervals
+# sns.lineplot(data=df, y="Eval Returns", x="N", hue='algorithm', style='algorithm', errorbar=("se", 1), alpha=0.001, legend="auto" if legend else False)
+sns.lineplot(data=df, y="Smoothed Returns", x="N", hue='algorithm', style='algorithm', errorbar=("se", 1), legend="auto" if legend else False)
+plt.savefig(runs_file[:-4] + "png")
+print(f"Saved results in {runs_file[:-4]}png .")
